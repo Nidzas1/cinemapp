5199209a78d0b18f5ed9fc76a459f9dc3ae114e9
dzoni <nidzaasa@gmail.com>
2023-02-13 14:23:37 +0100

    movie update page done

--- ap/src/pages/movies/Movie.jsx
+++ ap/src/pages/movies/Movie.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import '../movies/movie.scss'
 
 const Movie = () => {
 
@@ -46,62 +47,66 @@ const Movie = () => {
     }
     return (
         <>
-            {movie.map(m => (
-                <div className="login-box">
-                    <h2>{m.title}</h2>
-                    <form>
-                        <div className="user-box">
-                            <input type="text" onChange={e => setTitle(e.target.value)} />
-                            <label>Title</label>
-                        </div>
-                        <div className="user-box">
-                            <input type="number" onChange={e => setYear(e.target.value)} />
-                            <label>Year</label>
-                        </div>
-                        <div className="user-box">
-                            <input type="text" onChange={e => setDescription(e.target.value)} />
-                            <label>Description</label>
-                        </div>
-                        <div className="user-box">
-                            <input type="text" onChange={e => setImage(e.target.value)} />
-                            <label>Image</label>
-                        </div>
-                        <div className="user-box">
-                            <input type="text" onChange={e => setDuration(e.target.value)} />
-                            <label>Duration</label>
-                        </div>
-                        <div className="user-box">
-                            <input type="text" onChange={e => setPremiere(e.target.value)} />
-                            <label>Premiere</label>
-                        </div>
-                        <div className="user-box">
-                            <input type="text" onChange={e => setShowing(e.target.value)} />
-                            <label>Showing</label>
-                        </div>
-                        <div className="user-box">
-                            <input type="text" onChange={e => setTimePlaying(e.target.value)} />
-                            <label>Time playing</label>
-                        </div>
-                        <div className="user-box">
-                            <input type="text" onChange={e => setGenreId(e.target.value)} />
-                            <label>Genre</label>
-                        </div>
+            <div className='movie'>
+                {movie.map(m => (
+                    <div className="login-box">
+                        <h2>{m.title}</h2>
+                        <img src={`${m.image}`} />
+                        <form>
+                            <div className="user-box">
+                                <input type="text" onChange={e => setTitle(e.target.value)} />
+                                <label>Title</label>
+                            </div>
+                            <div className="user-box">
+                                <input type="number" onChange={e => setYear(e.target.value)} />
+                                <label>Year</label>
+                            </div>
+                            <div className="user-box">
+                                <input type="text" onChange={e => setDescription(e.target.value)} />
+                                <label>Description</label>
+                            </div>
+                            <div className="user-box">
+                                <input type="text" onChange={e => setImage(e.target.value)} />
+                                <label>Image</label>
+                            </div>
+                            <div className="user-box">
+                                <input type="text" onChange={e => setDuration(e.target.value)} />
+                                <label>Duration</label>
+                            </div>
+                            <div className="user-box">
+                                <input type="text" onChange={e => setPremiere(e.target.value)} />
+                                <label>Premiere</label>
+                            </div>
+                            <div className="user-box">
+                                <input type="text" onChange={e => setShowing(e.target.value)} />
+                                <label>Showing</label>
+                            </div>
+                            <div className="user-box">
+                                <input type="text" onChange={e => setTimePlaying(e.target.value)} />
+                                <label>Time playing</label>
+                            </div>
+                            <div className="user-box">
+                                <input type="text" onChange={e => setGenreId(e.target.value)} />
+                                <label>Genre</label>
+                            </div>
 
-                        <div className="user-box">
-                            <input type="text" onChange={e => setRoomId(e.target.value)} />
-                            <label>Room</label>
-                        </div>
-                        <a onClick={() => updateMovie(m.movie_id)}>
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                            Update
-                        </a>
-                    </form>
-                </div>
+                            <div className="user-box">
+                                <input type="text" onChange={e => setRoomId(e.target.value)} />
+                                <label>Room</label>
+                            </div>
+                            <a onClick={() => updateMovie(m.movie_id)}>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                Update
+                            </a>
+                        </form>
+                    </div>
 
-            ))}
+                ))}
+            </div>
+
         </>
 
     )
